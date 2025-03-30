47ff2fa2fd9d2bbd4fd8d5a3ca108fdf689f9856
Tilt384 <148476139+Tilt384@users.noreply.github.com>
2025-03-30 22:51:47 +0300

    colorsongsync

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const songCover = [
   "https://lh3.googleusercontent.com/jv4wTApw67zzZbF17bkcvdED-Spwt43Mb62sCZOLgGLvv517Mw4SCykS04kj-jMFy6P7LkyZuQMR3nwL=w544-h544-l90-rj",
   "https://lh3.googleusercontent.com/BBR2BBgn5JXKEm9gC3ddqKwyOJ3OsMZiWHTf-6BeBhoqzq0w5bay_p9H4xgXSZTj__74XvVFVMei4bOf=w544-h544-l90-rj"
 ];
-
+const colorThief = new ColorThief();
 
 const loadSong = (audioSrc, coverSrc) => {
   const audioSource = document.createElement("source");
@@ -30,6 +30,16 @@ const loadSong = (audioSrc, coverSrc) => {
   albumCoverImg.src = coverSrc;
 
   audioPlayer.load();
+  albumCoverImg.onload = () => {
+    const color = colorThief.getColor(albumCoverImg); 
+    const rgbColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    document.body.style.backgroundColor = rgbColor; 
+    document.querySelector(".player-container").style.backgroundColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.7)`;
+    document.querySelectorAll("button").forEach(btn => {
+      btn.style.backgroundColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.2)`;
+      btn.style.borderColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.5)`;
+    });
+  };
 };
 
 playPauseBtn.addEventListener("click", () => {
